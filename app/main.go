46cbfea43a900e1bd0625e1c31d168e2af1e0db8
Sonoko Mizuki <sonoko@mizuki.io>
2020-03-31 22:51:49 +0900

    add base code of app

--- app/main.go
+++ app/main.go
@@ -5,9 +5,16 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"fmt"
+
+	"github.com/google/go-github/v30/github"
 )
 
 func main() {
+	if eventType := os.Getenv("GITHUB_EVENT_NAME"); eventType != "push" {
+		log.Fatalf("Unsupported eventType(%s), only 'push' is supported", eventType)
+	}
+
 	// See: https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables
 	eventPath := os.Getenv("GITHUB_EVENT_PATH")
 	if eventPath == "" {
@@ -19,11 +26,12 @@ func main() {
 		log.Fatalf("To read file(path:%s) is failed err:%s ", eventPath, err)
 	}
 
-	// ToDo: define event typp, this is not good
-	var event map[string]interface{}
+	// https://github.com/google/go-github/blob/bf4e9281481bcbc811e5f8001c18cbe11613bffd/github/event_types.go
+	var event github.PushEvent
 	err = json.Unmarshal(payload, &event)
 	if err != nil {
 		log.Fatalf("To unmarshal payload is failed err:%s", err)
 	}
 
+	fmt.Printf("[test] commit is %s", event.HeadCommit.String())
 }
