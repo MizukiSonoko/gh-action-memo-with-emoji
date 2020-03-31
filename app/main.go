f58e8402fc284733ce0067b58872f4ccacccf526
Sonoko Mizuki <sonoko@mizuki.io>
2020-03-31 22:31:38 +0900

    add function get event from env

--- app/main.go
+++ app/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
 func main() {
-	print("Hello!!")
+	// See: https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables
+	eventPath := os.Getenv("GITHUB_EVENT_PATH")
+	if eventPath == "" {
+		log.Fatal("GITHUB_EVENT_PATH is empty, but it's invalid")
+	}
+
+	payload, err := ioutil.ReadFile(eventPath)
+	if err != nil {
+		log.Fatalf("To read file(path:%s) is failed err:%s ", eventPath, err)
+	}
+
+	// ToDo: define event typp, this is not good
+	var event map[string]interface{}
+	err = json.Unmarshal(payload, &event)
+	if err != nil {
+		log.Fatalf("To unmarshal payload is failed err:%s", err)
+	}
+
 }
